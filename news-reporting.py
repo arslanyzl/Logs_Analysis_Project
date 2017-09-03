ed195f3daf74c83672b8c6cf5443b45ee782b6e6
ARSLAN YAZLYYEV <30394242+arslanyzl@users.noreply.github.com>
2017-09-02 20:19:44 -0500

    Add files via upload

---
+++ news-reporting.py
@@ -0,0 +1,58 @@
+import psycopg2
+import datetime
+
+DB_NAME = "news"
+
+pop_article = (
+    "select title, count(*) as views "
+    "from articles join log on log.path = concat('/article/', articles.slug) "
+    "where log.status like '%200%' "
+    "group by title order by views desc limit 3")
+
+
+pop_author = (
+    "select authors.name, count(*) as views "
+    "from articles join authors on authors.id = articles.author "
+    "join log on log.path = concat('/article/', articles.slug) "
+    "where log.status like '%200%' group by name order by views desc")
+
+
+error_request = (
+    "select error_request.date, "
+    "(sum(error_request.views) * 100.0 /sum(total_request.views)) as error "
+    "from total_request join error_request on "
+    "total_request.date=error_request.date "
+    "group by error_request.date "
+    "having (sum(error_request.views) * 100.0 /sum(total_request.views))>1.00")
+
+
+def connect_result(query):
+
+    db = psycopg2.connect(database=DB_NAME)
+    c = db.cursor()
+    c.execute(query)
+    analys = c.fetchall()
+    db.close()
+    return analys
+
+
+def print_article(analys):
+    print ("\nI. Three most popular articles all time:\n")
+    for i in range(len(analys)):
+        print ("  " + str(i+1) + ". " + analys[i][0] + " - " + str(analys[i][1]) + " views")
+
+
+def print_author(analys):
+    print ("\nII. Most popular authors of articles all time:\n")
+    for i in range(len(analys)):
+        print ("  " + str(i+1) + ". " + analys[i][0] + " - " + str(analys[i][1]) + " views")
+
+
+def print_error(analys):
+    print ("\nIII. Most error occurred day all time:\n")
+    for i in analys:
+        print "  " + i[0].strftime('%B %d, %Y') + " - ", round(i[1], 2), "% errors"
+
+print_article(connect_result(pop_article))
+print_author(connect_result(pop_author))
+print_error(connect_result(error_request))
